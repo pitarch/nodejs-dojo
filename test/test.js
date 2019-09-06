1610482d2dca6528cd9736aee43f0c15309d69fa
Albert Pitarch <albert.pitarch@axonix.com>
2019-09-06 15:35:39 +0100

    add eslint config for vscode

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 describe('Array', () => {
     describe('#indexOf()', () => {
         it('should return -1 when the value is not present', () =>{
-            assert.equal([1,2,3].indexOf(4), -1)
+            assert.equal([1,2,3].indexOf(4), -1);
         }); 
     });
 });
