42233905a4c5d299905a0f960a6be568b1651637
Albert Pitarch <albert.pitarch@axonix.com>
2019-09-12 15:24:49 +0100

    first intro to typescript

---
+++ kata6/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module',
+    },
+    env: {
+        node: true,
+        es6: true,
+    }
+};
