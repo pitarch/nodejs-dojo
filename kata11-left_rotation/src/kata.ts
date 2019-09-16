8a0780cc5879f059d507f79628db949ed5005df6
Albert Pitarch <albert.pitarch@axonix.com>
2019-09-16 15:54:30 +0100

    improve performance on kata 11

--- kata11-left_rotation/src/kata.ts
+++ kata11-left_rotation/src/kata.ts
@@ -1,15 +1,20 @@
 // https://www.hackerrank.com/challenges/ctci-array-left-rotation
 function rotLeft(a: number[], d: number): number[] {
-  let i = 0
-  let value = a[0]
-  do {
-    const nextI = (i + a.length - d) % a.length
-    const backup = a[nextI]
-    a[nextI] = value
-    i = nextI
-    value = backup
-  } while (i != 0)
+  const aa = [...a]
+  const tail = a.slice(-d)
+  for (let i = 0, j = 0; i < a.length; i++) {
+    if (i < a.length - d) {
+      a[(i + a.length - d) % a.length] = a[i]
+    } else {
+      a[(i + a.length - d) % a.length] = tail[j++]
+    }
+  }
+  console.log(`[${aa}]/${d} => [${a}]`)
   return a
+}
+
+function rotLeft2(a: number[], d: number): number[] {
+  return [...a.slice(d), ...a.slice(0, d)]
 }
 
 export const fn = rotLeft
